561a41e259394529a2ee09763ada7428f7ffc5f2
nglaeser <nglaeser@users.noreply.github.com>
2019-03-25 14:45:15 -0400

    adding extra messages as log

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ app = Flask(__name__)
 def webhook():
   data = request.get_json()
 
+  send_message("calling parse_message")
   msg = parse_message(data)
+  send_message("received this message: {}".format(msg))
   if msg not None:
   # We don't want to reply to ourselves!
 #  if data['name'] != 'Log Reminder Beta':
@@ -22,8 +24,10 @@ def webhook():
   return "ok", 200
 
 def parse_message(data):
+  send_message("in parse_message method")
   # We don't want to reply to ourselves!
   if data['name'] != 'Log Reminder Beta':
+    send_message("message passes name restriction")
     msg = '{}, you sent "{}".'.format(data['name'], data['text'])
     send_message(msg)
     return msg
