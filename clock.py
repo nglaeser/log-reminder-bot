07716112509b2faa2321111c103231541387e8ee
nglaeser <nglaeser@users.noreply.github.com>
2019-03-25 15:36:15 -0400

    replace reminder time with the real time (sunday 11pm)

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import app
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=15, minute=35)
+@sched.scheduled_job('cron', day_of_week='sun', hour=22, minute=0)
 def scheduled_job():
   log_reminder()
 
